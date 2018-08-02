8d3197c0fdc6d16ac5978b4b0c9f2e9e42b1bcc5
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-08-02 09:59:39 +0300

    Dynamic_content_management_engine gem. Get instead of match method.

--- extensions/dynamic_content_management_engine/config/routes.rb
+++ extensions/dynamic_content_management_engine/config/routes.rb
@@ -2,6 +2,6 @@ DynamicContentManagementEngine::Engine.routes.draw do
   # Add your extension routes here
 	namespace :admin do
 		resources :dynamic_data
-		match "dynamic_data/picker_gallery/:gallery_id" => "posts#picker_gallery"
+		get "dynamic_data/picker_gallery/:gallery_id" => "posts#picker_gallery"
 	end
 end
