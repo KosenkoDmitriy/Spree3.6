64ddc6bc715ac7d54874a84362c83aacec7ce0d2
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-08-14 11:20:58 +0300

    added twitter initializer

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = "9FrtJ6Se5QVbcurg2gnB5g"
+  config.consumer_secret = "thpGs40ALvIxVWOUowAHWaPsmNu8RWewLMAeN5Doskc"
+  config.access_token = "3196831-Jv8TTcQvxkoG2swKvU59xcIUxOqp1bZq37dnRnT7g"
+  config.access_token_secret = "2pFRPRZcYu196SancREzzoESJBhzoSAkrtfTMmE7mE"
+end
