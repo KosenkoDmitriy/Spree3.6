c32f50a3ac0c29d0606c0ee10add6f11261b2faf
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-08-15 11:19:01 +0300

    Set default locale and other settings in application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,41 @@ module SoulPadSpree36
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    if ENV['LOCALE'] == 'en-GB'
+      config.time_zone = 'London'
+    elsif ENV['LOCALE'] == 'en-AU'
+      config.time_zone = 'Sydney'
+    elsif ENV['LOCALE'] == 'en-US'
+      config.time_zone = 'Eastern Time (US & Canada)'
+    end
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = ENV['LOCALE'] || 'en-GB'
+
+
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.prefix = "/assets"
+    config.serve_static_assets = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
+    config.action_mailer.default_url_options = {
+      :host => 'soulpad.co.uk'
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
