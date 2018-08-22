ef5b2a9df594a95680d78d6f9d1806ed3b386191
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-08-22 09:32:41 +0300

    Default missing image.

--- extensions/features_engine/app/models/feature.rb
+++ extensions/features_engine/app/models/feature.rb
@@ -3,15 +3,16 @@ class Feature < ActiveRecord::Base
   belongs_to :spree_product, :foreign_key => "product_id", :class_name => "Spree::Product"
 
   has_attached_file :attachment,
-                    :styles => { :thumbnail => "48x48>", :main => "940x325#", :shop_main => "700x325#", :shop_small => "340X325#"},
-                    :default_style => :main,
-                    :url => "/spree/features/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/spree/features/:id/:style/:basename.:extension"
+                    styles: { thumbnail: "48x48>", main: "940x325#", shop_main: "700x325#", shop_small: "340X325#" },
+                    default_style: :main,
+                    default_url: "/spree/features/:style/missing.png",
+                    url: "/spree/features/:id/:style/:basename.:extension",
+                    path: ":rails_root/public/spree/features/:id/:style/:basename.:extension"
   validate :no_attachment_errors
   # do_not_validate_attachment_file_type :attachment
   # validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\z/
   validates_attachment :attachment, content_type: { content_type: /\Aimage\/.*\z/ }
-  
+
 
   scope :enabled,   -> { where(enabled: true) }
   scope :shop_main, -> { where(shop_enabled: 'main') }
