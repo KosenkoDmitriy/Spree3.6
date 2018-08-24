fd2266732247dff8cc641180b44b4769755bee17
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-08-24 10:40:07 +0300

    Fixed error in taxon controller: undefined method `find_by_permalink!' for #<Class:0x007fd0a546a2a8> Did you mean? find_by_sql)

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -1,11 +1,12 @@
 Spree::TaxonsController.class_eval do
-  
+
   def show
-    @taxon = Spree::Taxon.find_by_permalink!(params[:id])
+    @taxon = Spree::Taxon.find_by!(permalink: params[:id])
     return unless @taxon
 
-    @searcher = build_searcher(params.merge(:taxon => @taxon.id))
-    @products = @searcher.retrieve_products.order("spree_variants.main_price ASC")
+    @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
+    @products = @searcher.retrieve_products#.order("spree_variants.main_price ASC")
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
   end
-  
+
 end
