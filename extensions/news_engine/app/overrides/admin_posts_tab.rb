9907daf195a5b2f3d3a314720c8086be791c5265
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-08-27 12:24:38 +0300

    news nav menu item with icon (left sidebar in the admin panel)

--- extensions/news_engine/app/overrides/admin_posts_tab.rb
+++ extensions/news_engine/app/overrides/admin_posts_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path  => "spree/layouts/admin",
 					:name => "admin_posts_tab",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab :news, :url => news_engine.admin_posts_url %>"
+                     :text => "<ul class='nav nav-sidebar'><%= tab :news, :url => news_engine.admin_posts_url, icon: 'tasks' %></ul>"
                      )
