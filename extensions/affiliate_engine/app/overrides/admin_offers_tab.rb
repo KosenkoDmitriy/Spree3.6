d5f3736adf7cdfaadd78a81689a68b9d519bee12
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-08-28 12:28:46 +0300

    Left nav sidebar with a link to offers.

--- extensions/affiliate_engine/app/overrides/admin_offers_tab.rb
+++ extensions/affiliate_engine/app/overrides/admin_offers_tab.rb
@@ -1,6 +1,6 @@
 #insert_after :admin_tabs, 'admin/offers/tab'
-Deface::Override.new(:virtual_path  => "spree/layouts/admin",
-						:name => "admin_offers_tab",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab :offers, :url => affiliate_engine.admin_offers_url %>"
-                     )
+Deface::Override.new(virtual_path: 'spree/layouts/admin',
+	name: 'admin_offers_tab',
+	insert_bottom: "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+	text: "<ul class='nav nav-sidebar'><%= tab :offers, url: affiliate_engine.admin_offers_url, icon: 'map-marker' %></ul>"
+)
