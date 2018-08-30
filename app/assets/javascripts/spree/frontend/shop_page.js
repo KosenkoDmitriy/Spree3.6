7572a528a985a2a13099502a4ab47863c86dcc2c
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-08-30 16:31:19 +0300

    fixed shop > sidebar > submenu defect after page reload

--- app/assets/javascripts/spree/frontend/shop_page.js
+++ app/assets/javascripts/spree/frontend/shop_page.js
@@ -1,12 +1,5 @@
 (function($){
   $(document).ready(function(){
-		//Category show/hide children categories
-		$('#product_nav li a.sub-menu').click(function() {
-			var parent = $(this).parents('li:eq(0)');
-			$(' > ul', parent).toggle();
-			$(this).toggleClass('current');
-			return false;
-		});
 
     //Contact form
 		$('form.contact_form').validate({
