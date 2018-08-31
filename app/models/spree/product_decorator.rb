c278e94431e7842109753403e09ab980bd423b01
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-08-31 15:41:52 +0300

    Product details > related products at the bottom

---
+++ app/models/spree/product_decorator.rb
@@ -0,0 +1,13 @@
+Spree::Product.class_eval do
+
+	# attr_accessible :description_details, :description_technique
+
+	validates_presence_of :tax_category_id, on: :update
+
+	scope :related_to, lambda{ |product|
+		# where("spree_products.id != :product_id", :product_id => product.id).order("RAND()").limit(4)
+		where("spree_products.id != :product_id", :product_id => product.id).order("RANDOM()").limit(4)
+		# where("spree_products.id != :product_id", product_id: product.id).order(rand(Spree::Product.count)).limit(4)
+	}
+
+end
