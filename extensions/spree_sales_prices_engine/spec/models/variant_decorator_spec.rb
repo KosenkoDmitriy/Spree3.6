aced10f0350f311187549ae34332b2240f0cda98
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-09-03 22:59:25 +0300

    Tests for variant decorator

--- extensions/spree_sales_prices_engine/spec/models/variant_decorator_spec.rb
+++ extensions/spree_sales_prices_engine/spec/models/variant_decorator_spec.rb
@@ -11,6 +11,8 @@ describe Spree::Variant do
       expect(result.variant_id).to eq(expected.variant_id)
       expect(result.amount.to_f).to eq(expected.amount.to_f)
       expect(result.currency).to eq(expected.currency)
+
+      expect(variant.sale_price.to_f).to eq(expected.amount.to_f)
     end
 
     it "returns the normal price if it is not on sale" do
@@ -25,7 +27,7 @@ describe Spree::Variant do
     end
 
     it "returns the main price if sale price is empty" do
-      variant = create(:variant, price: 15.00)
+      variant = create(:variant, main_price: 15.00, sale_price: '')
       expected = Spree::Price.new(variant_id: variant.id, currency: "USD", amount: variant.main_price)
 
       result = variant.price_in("USD")
@@ -33,6 +35,21 @@ describe Spree::Variant do
       expect(result.variant_id).to eq(expected.variant_id)
       expect(result.amount.to_f).to eq(expected.amount.to_f)
       expect(result.currency).to eq(expected.currency)
+
+      expect(variant.price.to_f).to eq(expected.amount.to_f)
+      expect(variant.main_price.to_f).to eq(expected.amount.to_f)
+      
+      expect(variant.main_price.to_f).to eq(variant.price.to_f)
     end
+
+    it "it is on sale ?" do
+      variant = create(:variant, sale_price: 15.00)
+      expected = Spree::Price.new(variant_id: variant.id, currency: "USD", amount: variant.main_price)
+
+      result = variant.is_on_sale?
+      expect(result).to eq(true)
+    end
+
+
   end
 end
