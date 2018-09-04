6d5a4d33ae2edfce23bb8b797351f290fb09d61e
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-09-04 22:22:54 +0300

    Deleted unused code (spree::image paperclip settings)

--- config/app_environment_variables.rb
+++ config/app_environment_variables.rb
@@ -1,1 +1,2 @@
+# use Paperclip (will be deprecated in Spree 4.0) instead of ActiveStorage
 ENV['SPREE_USE_PAPERCLIP'] = 'true'
--- extensions/watermark_engine/app/models/spree/image_decorator.rb
+++ extensions/watermark_engine/app/models/spree/image_decorator.rb
@@ -1,31 +1,4 @@
-# Spree::Image.class_eval do
-# # Spree::Asset.class_eval do
-# # has_one_attached :attachment
-# # has_attached_file :attachment
-#   # has_attached_file :attachment, styles: { mini: '100x100#'}
-# #   # validates_attachment :attachment, content_type: { content_type: /\Aimage\/.*\z/ }
-# #   #                   styles: { thumbnail: "48x48>", main: "940x325#", shop_main: "700x325#", shop_small: "340X325#" },
-# #   #                   default_style: :main,
-# #   #                   default_url: "/spree/features/:style/missing.png",
-# #   #                   url: "/spree/features/:id/:style/:basename.:extension",
-# #   #                   path: ":rails_root/public/spree/features/:id/:style/:basename.:extension"
-# #   has_attached_file(
-# #     :attachment,
-# #
-# #     )
-# #   # validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\z/
-#   # do_not_validate_attachment_file_type :attachment
-# #
-# end
-#
 Spree::Image.class_eval do
-  # has_attached_file :attachment, styles: { mini: '100x100#'}
-  # validates_attachment :attachment, content_type: { content_type: /\Aimage\/.*\z/ }
-  #                   styles: { thumbnail: "48x48>", main: "940x325#", shop_main: "700x325#", shop_small: "340X325#" },
-  #                   default_style: :main,
-  #                   default_url: "/spree/features/:style/missing.png",
-  #                   url: "/spree/features/:id/:style/:basename.:extension",
-  #                   path: ":rails_root/public/spree/features/:id/:style/:basename.:extension"
   has_attached_file(
     :attachment,
     :processors => [:thumbnail, :watermark],
@@ -65,5 +38,5 @@ Spree::Image.class_eval do
     :url => "/spree/products/:id/:style/:basename.:extension",
     :path => ":rails_root/public/spree/products/:id/:style/:basename.:extension"
     )
-  # validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\z/
 end
