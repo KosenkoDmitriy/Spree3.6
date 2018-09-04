b9283dc62bc052854d63eff577125572af7a449b
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-09-04 16:42:34 +0300

    deleted old sales prices engine

--- extensions/sales_prices_engine/app/overrides/admin_product_sale_price.rb
+++
@@ -1,5 +0,0 @@
-# insert_before :admin_product_form_right, "admin/products/price_additions"
-Deface::Override.new(:virtual_path  => "spree/admin/shared/tabs",
-                     :insert_after => "div#registration",
-                     :render => "admin/posts/tab",
-                     :name => "admin_posts_tab")
--- extensions/sales_prices_engine/config/routes.rb
+++
@@ -1,3 +0,0 @@
-SalesPricesEngine::Engine.routes.draw do
-  # Add your extension routes here
-end
