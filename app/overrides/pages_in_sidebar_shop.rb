afb255966ba12e433f17cb39a0b5f86a8f37e831
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-09-05 14:07:23 +0300

    Pages in the left sidebar of about and shop pages.

---
+++ app/overrides/pages_in_sidebar_shop.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+  virtual_path: 'shared/_categories',
+  name: 'pages_in_sidebar_shop',
+  insert_bottom: '#product_nav',
+  partial: 'spree/static_content/static_content_sidebar'
+)
