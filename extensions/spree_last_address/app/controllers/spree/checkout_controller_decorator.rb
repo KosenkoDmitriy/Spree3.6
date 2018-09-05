86c355d1b75e8c2b27eb706130ac774b3b052eb8
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-09-05 16:46:09 +0300

    Added lib and controller

--- extensions/spree_last_address/app/controllers/spree/checkout_controller_decorator.rb
+++ extensions/spree_last_address/app/controllers/spree/checkout_controller_decorator.rb
@@ -4,8 +4,8 @@ module Spree
   private
 
     def before_address
-			return if @order.bill_address or @order.ship_address
-			last_used_bill_address, last_used_ship_address = SpreeLastAddress.find_last_used_addresses(@order.email) unless @order.user.nil?
+  		return if @order.bill_address or @order.ship_address
+  		last_used_bill_address, last_used_ship_address = SpreeLastAddress.find_last_used_addresses(@order.email) unless @order.user.nil?
       preferred_bill_address, preferred_ship_address = spree_current_user.bill_address, spree_current_user.ship_address if spree_current_user && spree_current_user.respond_to?(:bill_address) && spree_current_user.respond_to?(:ship_address)
       @order.bill_address ||= preferred_bill_address || last_used_bill_address || Address.default
       @order.ship_address ||= preferred_ship_address || last_used_ship_address || Address.default
