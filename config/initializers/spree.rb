b0b7581bfc42695057d27d04da1b3e33819f7dc8
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-09-05 09:50:35 +0300

    undefined method `reduce' for :watermark:Symbol

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -18,39 +18,39 @@ end
 Spree.user_class = "Spree::User"
 
 # Spree::Image.attachment_definitions[:attachment][:processors] = [:thumbnail, :watermark]
-Spree::Image.attachment_definitions[:attachment][:processors] = :watermark
-Spree::Image.attachment_definitions[:attachment][:default_style] = :product
-Spree::Image.attachment_definitions[:attachment][:url] = "/spree/products/:id/:style/:basename.:extension", #':path'
-Spree::Image.attachment_definitions[:attachment][:path] = ":rails_root/public/spree/products/:id/:style/:basename.:extension"
-Spree::Image.attachment_definitions[:attachment][:styles] = {
-  :mini => {
-    :geometry => '48x48#',
-    :watermark_path => "#{Rails.root.to_s}/private/watermarks/mini.png",
-    :watermark_position => "SouthEast",
-    :format => :png,
-  },
-  :small => {
-    :geometry => '100x100#',
-    :watermark_path => "#{Rails.root.to_s}/private/watermarks/small.png",
-    :watermark_position => "SouthEast",
-    :format => :png,
-  },
-  :catalogue => {
-    :geometry => '220x180#',
-    :watermark_path => "#{Rails.root.to_s}/private/watermarks/catalogue.png",
-    :watermark_position => "SouthEast",
-    :format => :png,
-  },
-  :product => {
-    :geometry => '290x',
-    :watermark_path => "#{Rails.root.to_s}/private/watermarks/product.png",
-    :watermark_position => "SouthEast",
-    :format => :png,
-  },
-  :large => {
-    :geometry => '480x600>',
-    :watermark_path => "#{Rails.root.to_s}/private/watermarks/large.png",
-    :watermark_position => "SouthEast",
-    :format => :png,
-  },
-}
+# # Spree::Image.attachment_definitions[:attachment][:processors] = :watermark
+# Spree::Image.attachment_definitions[:attachment][:default_style] = :product
+# Spree::Image.attachment_definitions[:attachment][:url] = "/spree/products/:id/:style/:basename.:extension", #':path'
+# Spree::Image.attachment_definitions[:attachment][:path] = ":rails_root/public/spree/products/:id/:style/:basename.:extension"
+# Spree::Image.attachment_definitions[:attachment][:styles] = {
+#   :mini => {
+#     :geometry => '48x48#',
+#     :watermark_path => "#{Rails.root.to_s}/private/watermarks/mini.png",
+#     :watermark_position => "SouthEast",
+#     :format => :png,
+#   },
+#   :small => {
+#     :geometry => '100x100#',
+#     :watermark_path => "#{Rails.root.to_s}/private/watermarks/small.png",
+#     :watermark_position => "SouthEast",
+#     :format => :png,
+#   },
+#   :catalogue => {
+#     :geometry => '220x180#',
+#     :watermark_path => "#{Rails.root.to_s}/private/watermarks/catalogue.png",
+#     :watermark_position => "SouthEast",
+#     :format => :png,
+#   },
+#   :product => {
+#     :geometry => '290x',
+#     :watermark_path => "#{Rails.root.to_s}/private/watermarks/product.png",
+#     :watermark_position => "SouthEast",
+#     :format => :png,
+#   },
+#   :large => {
+#     :geometry => '480x600>',
+#     :watermark_path => "#{Rails.root.to_s}/private/watermarks/large.png",
+#     :watermark_position => "SouthEast",
+#     :format => :png,
+#   },
+# }
--- extensions/watermark_engine/app/models/spree/image_decorator.rb
+++ extensions/watermark_engine/app/models/spree/image_decorator.rb
@@ -1,42 +1,42 @@
 Spree::Image.class_eval do
-  # has_attached_file(
-  #   :attachment,
-  #   :processors => [:thumbnail, :watermark],
-    # :styles => {
-    #   :mini => {
-    #     :geometry => '48x48#',
-    #     :watermark_path => "#{Rails.root.to_s}/private/watermarks/mini.png",
-    #     :watermark_position => "SouthEast",
-    #     :format => :png,
-    #   },
-    #   :small => {
-    #     :geometry => '100x100#',
-    #     :watermark_path => "#{Rails.root.to_s}/private/watermarks/small.png",
-    #     :watermark_position => "SouthEast",
-    #     :format => :png,
-    #   },
-		# 	:catalogue => {
-		# 		:geometry => '220x180#',
-    #     :watermark_path => "#{Rails.root.to_s}/private/watermarks/catalogue.png",
-    #     :watermark_position => "SouthEast",
-    #     :format => :png,
-		# 	},
-    #   :product => {
-    #     :geometry => '290x',
-    #     :watermark_path => "#{Rails.root.to_s}/private/watermarks/product.png",
-    #     :watermark_position => "SouthEast",
-    #     :format => :png,
-    #   },
-    #   :large => {
-    #     :geometry => '480x600>',
-    #     :watermark_path => "#{Rails.root.to_s}/private/watermarks/large.png",
-    #     :watermark_position => "SouthEast",
-    #     :format => :png,
-    #   },
-    # },
-  #   :default_style => :product,
-  #   :url => "/spree/products/:id/:style/:basename.:extension",
-  #   :path => ":rails_root/public/spree/products/:id/:style/:basename.:extension"
-  #   )
+  has_attached_file(
+    :attachment,
+    :processors => [:thumbnail, :watermark],
+    :styles => {
+      :mini => {
+        :geometry => '48x48#',
+        :watermark_path => "#{Rails.root.to_s}/private/watermarks/mini.png",
+        :watermark_position => "SouthEast",
+        :format => :png,
+      },
+      :small => {
+        :geometry => '100x100#',
+        :watermark_path => "#{Rails.root.to_s}/private/watermarks/small.png",
+        :watermark_position => "SouthEast",
+        :format => :png,
+      },
+			:catalogue => {
+				:geometry => '220x180#',
+        :watermark_path => "#{Rails.root.to_s}/private/watermarks/catalogue.png",
+        :watermark_position => "SouthEast",
+        :format => :png,
+			},
+      :product => {
+        :geometry => '290x',
+        :watermark_path => "#{Rails.root.to_s}/private/watermarks/product.png",
+        :watermark_position => "SouthEast",
+        :format => :png,
+      },
+      :large => {
+        :geometry => '480x600>',
+        :watermark_path => "#{Rails.root.to_s}/private/watermarks/large.png",
+        :watermark_position => "SouthEast",
+        :format => :png,
+      },
+    },
+    :default_style => :product,
+    :url => "/spree/products/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/spree/products/:id/:style/:basename.:extension"
+    )
   # validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\z/
 end
