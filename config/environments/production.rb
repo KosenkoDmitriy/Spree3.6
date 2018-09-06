5391729e630bbc11769dd06890992c85a8aabfd2
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-09-06 13:07:58 +0300

    saas settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  # config.sass.preferred_syntax = :scss
+  # config.sass.cache = false
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
